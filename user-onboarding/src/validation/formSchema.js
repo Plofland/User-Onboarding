934f8b4908b1b8a059a402bef3c659e43e1ee7c5
Plofland <ploflan@gmail.com>
2020-12-16 20:02:29 -0500

    test commit

--- user-onboarding/src/validation/formSchema.js
+++ user-onboarding/src/validation/formSchema.js
@@ -5,7 +5,7 @@ export default yup.object().shape({
         .string()
         .required("username is required")
         .min(2, "username must be at least 3 characters long"),
-    email: yup.string().required()
+    email: yup.string().required("Must be a valid email")
     role: yup
     
 })
